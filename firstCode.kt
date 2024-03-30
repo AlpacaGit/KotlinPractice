c293a5bf85c3decd1449f7acb87cda6a019ee982
Alpaca <shn3.ucp@gmail.com>
2024-03-30 22:15:11 +0900

    関数を利用してみる

--- firstCode.kt
+++ firstCode.kt
@@ -2,4 +2,11 @@ fun main(){
     // 変数の宣言
     var text :String = "Hello! Kotlin!"
     println(text)
+    text = firstFunc()
+    println(text)
+}
+
+fun firstFunc(): String{
+    var text :String = "returnStr"
+    return text
 }
