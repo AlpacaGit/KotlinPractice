d22277ae2090d8a0000a9dd07103363f83a32f16
Alpaca <shn3.ucp@gmail.com>
2024-03-30 23:03:06 +0900

    最初のクラスを追加

--- firstCode.kt
+++ firstCode.kt
@@ -1,12 +1,23 @@
+package Models
 fun main(){
     // 変数の宣言
     var text :String = "Hello! Kotlin!"
     println(text)
     text = firstFunc()
     println(text)
+    // クラスの利用
+    var firstClass = FirstClass("これは最初のクラスだ")
+    firstClass.ShowName()
 }
 
 fun firstFunc(): String{
     var text :String = "returnStr"
     return text
+}
+
+// FirstClass
+class FirstClass(val name : String){
+    fun ShowName(){
+        println(name)
+    }
 }
